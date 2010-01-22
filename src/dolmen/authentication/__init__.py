d85fc16d2ad0fce543981559a2ff4cc2a90d1dfb
Souheil CHELFOUH <trollfot@vittra.org>
2010-01-22 21:08:47 +0100

    Added missing import

--- src/dolmen/authentication/__init__.py
+++ src/dolmen/authentication/__init__.py
@@ -2,6 +2,7 @@ from zope.i18nmessageid import MessageFactory
 MF = MessageFactory('dolmen.authentication')
 
 from dolmen.authentication.interfaces import (
-    IPrincipalFolder, IPrincipal, IGroup, IAccountStatus, IPasswordChecker)
+    IPrincipalFolder, IPrincipal, IGroup,
+    IAccountStatus, IPasswordChecker, IPasswordProtected)
 from dolmen.authentication.events import (
     IUserLoggedInEvent, IUserLoggedOutEvent, UserLoginEvent, UserLogoutEvent)
